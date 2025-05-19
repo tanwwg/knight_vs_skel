77d7b0eb23c7d3d10d8bf5e501bd724252539c33
Tan Thor Jen <tthorjen@gmail.com>
2025-05-19 09:56:03 +0800

    add skeleton archer

---
+++ Assets/ShootProjectile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public Transform target;
+    public GameObject prefab;
+    public float force;
+
+    public void Shoot()
+    {
+        var arrow = Instantiate(prefab, prefab.transform.position, Quaternion.identity);
+        arrow.gameObject.SetActive(true);
+        
+        var body = arrow.GetComponent<Rigidbody2D>();
+        var direction = target.position - prefab.transform.position;
+        Debug.LogFormat("target {0} - prefab {1} = {2}", target.position, prefab.transform.position, direction);
+        
+        body.linearVelocity = direction.normalized * force;
+        
+        // body.AddForce(direction.normalized * force, ForceMode2D.Impulse);
+        
+        var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        arrow.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+}
