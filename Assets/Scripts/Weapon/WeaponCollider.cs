b8f9d7a1c63d89c53dd80db6069379efd39e1658
Tan Thor Jen <tthorjen@gmail.com>
2025-05-20 10:27:48 +0800

    fix weapon collider check

--- Assets/Scripts/Weapon/WeaponCollider.cs
+++ Assets/Scripts/Weapon/WeaponCollider.cs
@@ -1,15 +1,27 @@
 using System;
 using UnityEngine;
 
+public static class LayerMaskExtension
+{
+    public static bool IsInMask(this LayerMask mask, int layer)
+    {
+        // Check if the object's layer matches the mask using bitwise AND
+        return (mask.value & (1 << layer)) != 0;
+    }
+}
+
 public class WeaponCollider : MonoBehaviour
 {
     public Hittable owner;
+    public LayerMask hitLayer;
     
     public int damage;
     public float knockback;
     
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (!hitLayer.IsInMask(other.gameObject.layer)) return;
+        
         // Debug.Log("WeaponCollider " + other.gameObject.name);
         var hit = other.gameObject.GetComponent<Hittable>();
         if (hit && hit != owner)
